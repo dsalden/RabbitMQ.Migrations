5da4053760e324b568ca8ee5ff919e46ae414d40
dsalden <dominiquesalden@gmail.com>
2019-09-30 16:29:13 +0200

    Added dependency injection

---
+++ RabbitMQ.Migrations/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RabbitMQ.Migrations.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRabbitMqMigrations(this IServiceCollection services)
+        {
+            services.AddTransient<IRabbitMqMigrator, RabbitMqMigrator>();
+            services.AddTransient<IRabbitMqHistory, RabbitMqHistory>();
+
+            return services;
+        }
+    }
+}
